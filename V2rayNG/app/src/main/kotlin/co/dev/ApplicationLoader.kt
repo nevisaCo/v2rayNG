ae3356ed9802e646f02d6ec1243c17341d10d21a
fereidoon Savadkoohi <fereidoonsavadkoohi@gmail.com>
2023-01-13 13:10:51 +0330

    update nevisa libe

--- V2rayNG/app/src/main/kotlin/co/dev/ApplicationLoader.kt
+++ V2rayNG/app/src/main/kotlin/co/dev/ApplicationLoader.kt
@@ -3,17 +3,32 @@ package co.dev
 import android.util.Log
 import co.dev.flurry.FlurryHelper
 import co.nevisa.commonlib.ApplicationLoader
-import com.v2ray.ang.BuildConfig
+import co.nevisa.commonlib.volley.ILoginNeedCallback
+import co.nevisa.commonlib.volley.VolleyHelper
+import com.v2ray.ang.BuildConfig.*
 import com.v2ray.ang.R
+import java.util.Objects
 
 open class ApplicationLoader : ApplicationLoader() {
     override fun onCreate() {
         super.onCreate()
         Log.i(Config.TAG, "onCreate: ")
 
-        super.init(R.drawable.ic_stat_name, BuildConfig.DEBUG_VERSION, BuildConfig.FLURRY_APP_ID)
+        super.init(
+            R.drawable.ic_stat_name,
+            DEBUG_VERSION,
+            FLURRY_APP_ID,
+            APPLICATION_ID
+        )
 
         FlurryHelper().init(this)
 
+        val callback = ILoginNeedCallback {
+
+        }
+
+        VolleyHelper.getInstance().init("status", "data", "", false, callback);
     }
+
+
 }
